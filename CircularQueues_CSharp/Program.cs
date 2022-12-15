9b80cdf1a0dbb754f2dcfeac374a01eb2f712cac
Dianitsya <dianitasyanandamasta@gmail.com>
2022-12-15 16:41:45 +0700

    add class Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -12,6 +12,97 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            //This statement checks for thr overflow condition.
+            if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            //The following statement checks whether the queue is empty. if the queue is empty,then the value of the REAR and FRONT variables is set to 0.
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                //if REAR is at the last position of the array, then the value of REAR is set to 0 that corresponds to the first position of the array.
+                if (REAR == max -1)
+                    REAR = 0;
+                else
+                    //if REAR is not at the last position, then its value is incremented by one.
+                    REAR = REAR + 1;
+            }
+            //Once the position of REAR is determined, the element is added at ist proper place.
+            queue_array[REAR] = element;
+        }
+        public void remove()
+        {
+            //Checks whether the queue is empty.
+            if(FRONT == -1)
+            {
+                Console.WriteLine("Queue underflow\n");
+                return ;
+            }
+            Console.WriteLine("\nThe element deleted from the queue is: " + queue_array[FRONT] + "\n");
+            //Checks if the queue has no element.
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                //If the element to be deleted is at the last position of the array then the value of FRONT is set to 0 i.e to the first element of the array.
+                if (FRONT == max -1)
+                    FRONT = 0;
+                else
+                    //FRONT is incremented by one if it is not the first element of array
+                    FRONT = FRONT + 1;
+            }
+        }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            //checks if the queue is empty
+            if (FRONT == 1)
+            {
+                Console.WriteLine("Queu is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElements in the queue are....................\n");
+            if (FRONT_position <= REAR_position)
+            {
+                //Traverses the queue till the last element present in an array.
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + " ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                //Traverses the queue till the last position of the array.
+                while (FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + " ");
+                    FRONT_position++;
+                }
+                //set the FRONT position to the first element of the array.
+                FRONT_position = 0;
+                //traverses the array till the last element present in the queue.
+                while(FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + " ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
         }
